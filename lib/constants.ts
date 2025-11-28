60b0022c9cad9afe26552ab18c8ce91fc19d100b
Jobair Al Sarkar <jobairalsarkar1@gmail.com>
2025-11-28 20:35:59 +0600

    chore: added necessary constants including image files

---
+++ lib/constants.ts
@@ -0,0 +1,40 @@
+import { BarChart3, Cpu, Globe, Laptop, Smartphone, Sparkle } from "lucide-react";
+
+export const services = [
+  {
+    "id": 1,
+    "title": "Mobile App Design",
+    "description": "Delivering intuitive and visually polished mobile interfaces...",
+    "icon": Smartphone
+  },
+  {
+    "id": 2,
+    "title": "Website Design",
+    "description": "Designing modern, responsive, and user-centered websites...",
+    "icon": Globe
+  },
+  {
+    "id": 3,
+    "title": "Dashboard Design",
+    "description": "Creating clear, data-driven dashboards with meaningful insights...",
+    "icon": BarChart3
+  },
+  {
+    "id": 4,
+    "title": "Landing Page Design",
+    "description": "Building high-converting landing pages with clean layouts...",
+    "icon": Laptop
+  },
+  {
+    "id": 5,
+    "title": "SaaS Product Design",
+    "description": "Designing scalable SaaS interfaces that balance usability and functionality...",
+    "icon": Cpu
+  },
+  {
+    "id": 6,
+    "title": "App/Website Redesign",
+    "description": "Revamping outdated products with a fresh, modern visual direction...",
+    "icon": Sparkle
+  }
+]
